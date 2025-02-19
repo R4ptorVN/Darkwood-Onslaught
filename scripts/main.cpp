83b0662590e7acaaaa45f3ec1f8f9b993cae26c2
Terrobyte <phatthehell123@gmail.com>
2025-02-19 13:29:50 +0700

    beo

---
+++ scripts/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <SDL.h>
+#include <SDL_image.h>
+
+using namespace std;
+
+#include <RenderWindow.hpp>
+
+int main(int argc, char* args [])
+{
+
+    if (SDL_Init(SDL_INIT_VIDEO))
+        SDL_Log("Failed to initialize SDL2");
+    if (!(IMG_Init(IMG_INIT_PNG)))
+        SDL_Log("Failed to initialize SDL2_image");
+
+    RenderWindow window("GAME v1.0", 1280, 720);
+
+    SDL_Texture* grassTexture = window.loadTexture("resources/grass.png");
+
+    if (grassTexture == NULL)
+        SDL_Log("Failed to load texture");
+
+    bool gameRunning = true;
+
+    SDL_Event event;
+
+    while (gameRunning)
+    {
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_QUIT)
+                gameRunning = false;
+        }
+
+        window.clear();
+        window.render(grassTexture);
+        window.display();
+    }
+
+    window.cleanUp();
+    SDL_Quit();
+    return 0;
+}
